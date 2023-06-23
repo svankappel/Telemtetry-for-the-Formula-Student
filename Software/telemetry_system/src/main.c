dd7438356cbfacbd3a8a3c7a9b691350330f32ee
svankappel <sylvestrevk@gmail.com>
2023-06-23 09:26:03 +0200

    UDP working

--- Software/telemetry_system/src/main.c
+++ Software/telemetry_system/src/main.c
@@ -22,17 +22,11 @@ int main(void)
 	nrfx_clock_divider_set(NRF_CLOCK_DOMAIN_HFCLK, NRF_CLOCK_HFCLK_DIV_1);			//set 128 MHZ clock
 
 	Task_Wifi_Sta_Init();
-	//Task_UDP_Client_Init();
+	Task_UDP_Client_Init();
 
 	k_msleep(10000);
 	
-	int udpClientSocket; 
-	udpClientSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
-	if ( udpClientSocket < 0 ) {
-		LOG_ERR( "UDP Client error: socket: %d\n", errno );
-		k_sleep( K_FOREVER );
-	}
+	
 
 	k_sleep( K_FOREVER );
 	
