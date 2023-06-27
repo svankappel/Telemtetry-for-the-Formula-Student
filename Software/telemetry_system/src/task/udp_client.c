86f361d596141f5ca5db9c9cd585a79dd7c74e9a
svankappel <sylvestrevk@gmail.com>
2023-06-27 11:07:57 +0200

    udp bugfix + code cleaned

--- Software/telemetry_system/src/task/udp_client.c
+++ Software/telemetry_system/src/task/udp_client.c
@@ -1,13 +1,6 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(sta, LOG_LEVEL_DBG);
 
-
-#define CONFIG_NET_CONFIG_PEER_IPV4_ADDR1 "192.168.50.110"
-#define CONFIG_NET_CONFIG_PEER_IPV4_ADDR2 "192.168.43.205"
-
-#define UDP_CLIENT_PORT1 1502
-#define UDP_CLIENT_PORT2 1502
-
 #include <zephyr/kernel.h>
 #include <errno.h>
 #include <stdio.h>
@@ -20,15 +13,19 @@ LOG_MODULE_DECLARE(sta, LOG_LEVEL_DBG);
 #include "udp_client.h"
 #include "deviceinformation.h"
 
+
+
+
+
 //! Stack size for the UDP_SERVER thread
 #define UDP_CLIENT_STACK_SIZE 2048
 //! UDP_SERVER thread priority level
 #define UDP_SERVER_PRIORITY 5
 //! Time in miliseconds to wait to send the UDP message since the board 
 // gets a stable IP address
-#define UDP_CLIENT_WAIT_TO_SEND_MS 1000
+#define UDP_CLIENT_WAIT_TO_SEND_MS 500
 
-
+#define SOCKET_NUMBER 2
 
 //! UDP client connection check interval in miliseconds
 #define UDP_CLIENT_SLEEP_TIME_MS 100
@@ -38,6 +35,12 @@ LOG_MODULE_DECLARE(sta, LOG_LEVEL_DBG);
 K_THREAD_STACK_DEFINE(UDP_CLIENT_STACK, UDP_CLIENT_STACK_SIZE);
 //! Variable to identify the UDP Client thread
 static struct k_thread udpClientThread;
+
+
+
+
+
+
 
 //! UDP message sent by the client.
 static const uint8_t udpTestMessage[] =
@@ -64,6 +67,15 @@ static const uint8_t udpTestMessage[] =
 
 void connectUDPSocket(int * udpClientSocket,struct sockaddr_in * serverAddress)
 {
+	// Client socket creation 
+	*udpClientSocket = socket(serverAddress->sin_family, SOCK_DGRAM, IPPROTO_UDP );
+
+	if ( *udpClientSocket < 0 ) 
+	{
+		LOG_ERR( "UDP Client error: socket: %d\n", errno );
+		k_sleep( K_FOREVER );
+	}
+
 	// Connection to the server. 
 	int connectionResult = connect(*udpClientSocket, ( struct sockaddr * )serverAddress, sizeof( *serverAddress ));
 
@@ -83,93 +95,72 @@ void connectUDPSocket(int * udpClientSocket,struct sockaddr_in * serverAddress)
 *		configuration file as Peer address.
 * 		This function is used on an independent thread.
 */
-void UDP_Client() {
-    int udpClientSocket1;
-	int udpClientSocket2;
-	
-	struct sockaddr_in serverAddress1;
-	struct sockaddr_in serverAddress2;
+void UDP_Client() 
+{
+	char* addresses[SOCKET_NUMBER] = {"192.168.50.110","192.168.43.205"};
+	int ports[SOCKET_NUMBER] = {1502,1502};
 
-	int sentBytes1 = 0;
-	int sentBytes2 = 0;
+
+    int udpClientSocket[SOCKET_NUMBER];
 	
+	struct sockaddr_in serverAddress[SOCKET_NUMBER];
+
+	int sentBytes[2];
+
 	// Starve the thread until a DHCP IP is assigned to the board 
 	while(!context.ip_assigned){
 		k_msleep( UDP_CLIENT_SLEEP_TIME_MS );
 	}
 
-	// Server IPV4 address configuration 
-	serverAddress1.sin_family = AF_INET;
-	serverAddress1.sin_port = htons( UDP_CLIENT_PORT1 );
-	inet_pton(AF_INET, CONFIG_NET_CONFIG_PEER_IPV4_ADDR1, &serverAddress1.sin_addr );
+	for(int i=0;i<SOCKET_NUMBER;i++)
+	{
+		sentBytes[i]=0;
 
-	// Client socket creation 
-	udpClientSocket1 = socket(serverAddress1.sin_family, SOCK_DGRAM, IPPROTO_UDP );
+		// Server IPV4 address configuration 
+		serverAddress[i].sin_family = AF_INET;
+		serverAddress[i].sin_port = htons(ports[i]);
+		inet_pton(AF_INET, addresses[i], &serverAddress[i].sin_addr );
 
-	if ( udpClientSocket1 < 0 ) {
-		LOG_ERR( "UDP Client error: socket: %d\n", errno );
-		k_sleep( K_FOREVER );
+		connectUDPSocket(&udpClientSocket[i],&serverAddress[i]);
 	}
 
-	// Server IPV4 address configuration 
-	serverAddress2.sin_family = AF_INET;
-	serverAddress2.sin_port = htons( UDP_CLIENT_PORT2 );
-	inet_pton(AF_INET, CONFIG_NET_CONFIG_PEER_IPV4_ADDR2, &serverAddress2.sin_addr );
-
-	// Client socket creation 
-	udpClientSocket2 = socket(serverAddress2.sin_family, SOCK_DGRAM, IPPROTO_UDP );
-
-	if ( udpClientSocket2 < 0 ) {
-		LOG_ERR( "UDP Client error: socket: %d\n", errno );
-		k_sleep( K_FOREVER );
-	}
-
-	k_msleep( UDP_CLIENT_WAIT_TO_SEND_MS );
-
-	//connect socket
-	connectUDPSocket(&udpClientSocket1,&serverAddress1);
-	connectUDPSocket(&udpClientSocket2,&serverAddress2);
+	k_msleep(UDP_CLIENT_WAIT_TO_SEND_MS);
 
 	while(true)
 	{
 		//if wifi connection is lost
 		if(!context.ip_assigned)
 		{
-			close(udpClientSocket1);		//close socket
-			close(udpClientSocket2);		//close socket
+			for(int i=0;i<SOCKET_NUMBER;i++)
+				close(udpClientSocket[i]);		//close sockets
+
 
 			// Starve the thread until a DHCP IP is assigned to the board 
 			while( !context.ip_assigned ){
 				k_msleep( UDP_CLIENT_SLEEP_TIME_MS );
 			}
 
-			k_msleep( UDP_CLIENT_WAIT_TO_SEND_MS );
+			
+			for(int i=0;i<SOCKET_NUMBER;i++)
+				connectUDPSocket(&udpClientSocket[i],&serverAddress[i]); // reconnect the socket
 
-			connectUDPSocket(&udpClientSocket1,&serverAddress1); // reconnect the socket
-			connectUDPSocket(&udpClientSocket2,&serverAddress2); // reconnect the socket
+			k_msleep( UDP_CLIENT_WAIT_TO_SEND_MS );
 		}
 		else // if wifi connected
 		{
-			// Send the udp message 
-			sentBytes1 = send(udpClientSocket1, udpTestMessage, sizeof(udpTestMessage), 0);
-			sentBytes2 = send(udpClientSocket2, udpTestMessage, sizeof(udpTestMessage), 0);
-
-			LOG_INF( "UDP 1 Client mode. Sent: %d", sentBytes1);
-			if ( sentBytes1 < 0 ) 
+			for(int i=0;i<SOCKET_NUMBER;i++)
 			{
-				LOG_ERR( "UDP 1 Client error: send: %d\n", errno );
-				close(udpClientSocket1);
-				LOG_ERR( "UDP 1 Client error Connection from closed\n" );
-			}
+				// Send the udp message 
+				sentBytes[i] = send(udpClientSocket[i], udpTestMessage, sizeof(udpTestMessage), 0);
 
-			LOG_INF( "UDP 2 Client mode. Sent: %d", sentBytes2);
-			if ( sentBytes2 < 0 ) 
-			{
-				LOG_ERR( "UDP 2 Client error: send: %d\n", errno );
-				close(udpClientSocket2);
-				LOG_ERR( "UDP 2 Client error Connection from closed\n" );
+				LOG_INF( "UDP %d Client mode. Sent: %d", i,sentBytes[i]);
+				if ( sentBytes[i] < 0 ) 
+				{
+					LOG_ERR( "UDP %d Client error: send: %d\n", i,errno );
+					close(udpClientSocket[i]);
+					LOG_ERR( "UDP %d Client error Connection from closed\n",i );
+				}
 			}
-
 			k_msleep(500);
 		}
 	}
