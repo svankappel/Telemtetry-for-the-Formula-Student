0116070a3708ed3cab88690f143c3abf35907106
svankappel <sylvestrevk@gmail.com>
2023-06-30 17:30:06 +0200

    data sender on sensor buffer with config file names

--- Software/telemetry_system/src/task/data_sender.c
+++ Software/telemetry_system/src/task/data_sender.c
@@ -20,79 +20,42 @@ LOG_MODULE_DECLARE(sta, LOG_LEVEL_DBG);
 //! data sender stack definition
 K_THREAD_STACK_DEFINE(DATA_SENDER_STACK, DATA_SENDER_STACK_SIZE);
 
-
-
 //! Variable to identify the data sender thread
 static struct k_thread dataSenderThread;
-
-struct data {
-	uint32_t TensionBatteryHV;
-	uint32_t AmperageBatteryHV;
-	uint32_t TemperatureBatteryHV;
-	uint32_t EnginePower;
-	uint32_t EngineTemperature;
-	uint32_t EngineAngularSpeed;
-	uint32_t CarSpeed;
-	uint32_t PressureTireFL;
-	uint32_t PressureTireFR;
-	uint32_t PressureTireBL;
-	uint32_t PressureTireBR;
-	uint32_t InverterTemperature;
-	uint32_t TemperatureBatteryLV;
-};
-
-static const struct json_obj_descr data_descr[] = {
-  JSON_OBJ_DESCR_PRIM(struct data, TensionBatteryHV, JSON_TOK_NUMBER),
-  JSON_OBJ_DESCR_PRIM(struct data, AmperageBatteryHV, JSON_TOK_NUMBER),
-  JSON_OBJ_DESCR_PRIM(struct data, TemperatureBatteryHV, JSON_TOK_NUMBER),
-  JSON_OBJ_DESCR_PRIM(struct data, EnginePower, JSON_TOK_NUMBER),
-  JSON_OBJ_DESCR_PRIM(struct data, EngineTemperature, JSON_TOK_NUMBER),
-  JSON_OBJ_DESCR_PRIM(struct data, EngineAngularSpeed, JSON_TOK_NUMBER),
-  JSON_OBJ_DESCR_PRIM(struct data, CarSpeed, JSON_TOK_NUMBER),
-  JSON_OBJ_DESCR_PRIM(struct data, PressureTireFL, JSON_TOK_NUMBER),
-  JSON_OBJ_DESCR_PRIM(struct data, PressureTireFR, JSON_TOK_NUMBER),
-  JSON_OBJ_DESCR_PRIM(struct data, PressureTireBL, JSON_TOK_NUMBER),
-  JSON_OBJ_DESCR_PRIM(struct data, PressureTireBR, JSON_TOK_NUMBER),
-  JSON_OBJ_DESCR_PRIM(struct data, InverterTemperature, JSON_TOK_NUMBER),
-  JSON_OBJ_DESCR_PRIM(struct data, TemperatureBatteryLV, JSON_TOK_NUMBER),
-};
-		
+	
 
 void Data_Sender() 
 {
 	
-	
-	struct data myData;
-	
 	while(true)
 	{
-		myData.TensionBatteryHV= sys_rand32_get()%600;
-		myData.AmperageBatteryHV=sys_rand32_get()%200;
-		myData.TemperatureBatteryHV=sys_rand32_get()%80;
-		myData.EnginePower=sys_rand32_get()%35000;
-		myData.EngineTemperature=sys_rand32_get()%80;
-		myData.EngineAngularSpeed=sys_rand32_get()%2300;
-		myData.CarSpeed=sys_rand32_get()%120;
-		myData.PressureTireFL=sys_rand32_get()%6;
-		myData.PressureTireFR=sys_rand32_get()%6;
-		myData.PressureTireBL=sys_rand32_get()%6;
-		myData.PressureTireBR=sys_rand32_get()%6;
-		myData.InverterTemperature=sys_rand32_get()%80;
-		myData.TemperatureBatteryLV=sys_rand32_get()%80;
+		for(int i=0; i<configFile.sensorNumber;i++)
+		{
+			sensorBuffer[i].value=sys_rand32_get()%100;
+		}
 
-		
 		if(context.ip_assigned)
 		{
 			char * memPtr = k_heap_alloc(&messageHeap,configFile.TelemetyDataSize,K_NO_WAIT);
 
-
-
 			if(memPtr != NULL)			//memory alloc success
 			{
+				sprintf(memPtr,"{");
+				bool first = true;
+				for(int i=0; i<configFile.sensorNumber;i++)
+				{
+					if(sensorBuffer[i].live)
+					{
+						if(first)
+							sprintf(memPtr,"%s\"%s\":%d",memPtr,sensorBuffer[i].name,sensorBuffer[i].value);
+						else
+							sprintf(memPtr,"%s,\"%s\":%d",memPtr,sensorBuffer[i].name,sensorBuffer[i].value);
+						first=false;
+					}
+				}
+				strcat(memPtr,"}");
 
-				json_obj_encode_buf(data_descr,ARRAY_SIZE(data_descr),&myData,memPtr,configFile.TelemetyDataSize);		//put json in allocated memory
 				k_queue_append(&udpQueue,memPtr);
-
 			}
 			else //mem alloc fail
 			{
