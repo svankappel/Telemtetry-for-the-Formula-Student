20f6a0b28dd424e20f2efffa41d57fc7ab509907
svankappel <sylvestrevk@gmail.com>
2024-05-17 22:45:00 +0200

    receive date and time (problem with wifi)

--- Software/telemetry_system_transmitter/src/main.c
+++ Software/telemetry_system_transmitter/src/main.c
@@ -29,10 +29,8 @@
 #include "task/udp_client.h"
 #include "task/led.h"
 #include "task/data_sender.h"
-//#include "task/data_logger.h"
 #include "task/memory_management.h"
 #include "task/config_read.h"
-//#include "task/button_manager.h"
 #include "task/can_controller.h"
 #include "task/gps_controller.h"
 
@@ -54,7 +52,6 @@ int main(void)
 
 	if(ret == 0)			//configuration file ok
 	{
-
 		Task_Wifi_Sta_Init();			//start wifi stationning Thread
 		
 		Task_UDP_Client_Init();			//start udp client Thread
